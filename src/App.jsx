a587cf33ac1b322be83e11480a0531c71588d540
shekhmohibur <mohibur.rahman2003@gmail.com>
2025-08-15 19:43:38 +0600

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+import Blogs from './components/blogs/Blogs'
+import Header from './components/header/Header'
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-blue-500 text-center'>Vite + React</h1>
+      <h1 className='text-3xl text-blue-500 text-center'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
